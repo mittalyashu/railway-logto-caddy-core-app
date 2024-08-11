4cd8bd4c201122f57f1ba6c3d40223744cdf7f17
Yashu Mittal <mittalyashu77@gmail.com>
2024-08-11 18:42:36 +0000

    fix: typo in variable "LOGTO_PRIVATE_DOMAIN"

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 # for backwards compatibility, seperates host and port from url
-export LOGO_PRIVATE_DOMAIN=${LOGO_PRIVATE_DOMAIN:-${FRONTEND_HOST%:*}}
+export LOGTO_PRIVATE_DOMAIN=${LOGTO_PRIVATE_DOMAIN:-${FRONTEND_HOST%:*}}
 export CORE_APP_PORT=${CORE_APP_PORT:-${FRONTEND_HOST##*:}}
 
-echo using frontend: ${LOGO_PRIVATE_DOMAIN} with port: ${CORE_APP_PORT}
+echo using frontend: ${LOGTO_PRIVATE_DOMAIN} with port: ${CORE_APP_PORT}
 
 exec caddy run --config Caddyfile --adapter caddyfile 2>&1
